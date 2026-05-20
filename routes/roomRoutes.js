c5d65d9b80b38412cb5c33c64f56494be06a11c6
Dinesh756 <nalliboinadinesh@gmail.com>
2026-05-20 20:19:35 +0530

    ...

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const { createRoom, getRoomsByHostel, getRoomsByFloor, getRoomById, updateRoom, deleteRoom } = require('../controller/roomController');
 const { protect } = require('../middleware/authMiddleware');
 
+// router.post('/create', protect, createRoom);
+// router.get('/:hostelId', protect, getRoomsByHostel);
+// router.get('/floor/:floorId', protect, getRoomsByFloor);
+// router.get('/single/:roomId', protect, getRoomById);
+// router.put('/:roomId', protect, updateRoom);
+// router.delete('/:roomId', protect, deleteRoom);
+
 router.post('/create', protect, createRoom);
-router.get('/:hostelId', protect, getRoomsByHostel);
-router.get('/floor/:floorId', protect, getRoomsByFloor);
-router.get('/single/:roomId', protect, getRoomById);
+router.get('/:hostelId', getRoomsByHostel);
+router.get('/floor/:floorId', getRoomsByFloor);
+router.get('/single/:roomId', getRoomById);
 router.put('/:roomId', protect, updateRoom);
 router.delete('/:roomId', protect, deleteRoom);
-
 module.exports = router;
