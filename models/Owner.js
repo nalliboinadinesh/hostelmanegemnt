ebadd258bdb3c0ac135489c52b24a91aaaacc887
Dinesh756 <nalliboinadinesh@gmail.com>
2026-06-22 19:09:00 +0530

    fixed the 8 bugs

--- models/Owner.js
+++ models/Owner.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const ownerSchema = new mongoose.Schema({
   ownerNumber: { type: String, required: true, unique: true },
-  ownerName:   { type: String, default: null },
-  email:       { type: String, default: null },
-  isExisted:   { type: Boolean, default: false },
+  isExisted: { type: Boolean, default: false },
 }, { timestamps: true, collection: 'owners' });
 
 module.exports = mongoose.model('Owner', ownerSchema);
